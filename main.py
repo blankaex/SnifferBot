006f2772ed0518347bfc47358a3944e377230cf9
blankaex <blankaex@protonmail.com>
2019-08-22 07:26:00 +0000

    added signal handler to shut down bot

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
+import signal, sys
 from snifferbot import *
+
+def signalHandler(sig, frame):
+    print("Exiting...")
+    sys.exit(0)
+
+signal.signal(signal.SIGINT, signalHandler)
 
 bot = snifferbot()
 with open('token', 'r') as token:
