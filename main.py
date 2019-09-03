c935524bf7e8af7e6cec03e794370285289eac3d
blankaex <blankaex@protonmail.com>
2019-09-03 03:12:19 +0000

    fixed typo

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ bot = snifferbot()
 
 try:
     main_loop = asyncio.get_event_loop()
-    with open('token/discord', 'r') as token:
+    with open('tokens/discord', 'r') as token:
         main_loop.run_until_complete(bot.start(token.read()))
 except KeyboardInterrupt:
     main_loop.run_until_complete(bot.logout())
